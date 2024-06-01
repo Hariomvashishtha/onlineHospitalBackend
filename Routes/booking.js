8b8a36ea6d816768062f752b4bdd65de71d0ad36
Hariom Vashishtha <hariom.vashishtha@MB-M-HARIOMV.local>
2024-06-02 01:56:29 +0530

    booking allowed for doctor role

--- Routes/booking.js
+++ Routes/booking.js
@@ -3,5 +3,5 @@ import { authenticate, restrict } from "../auth/verifyToken.js";
 import {getCheckoutSession} from "../Controllers/bookingController.js";
 
 const router = express.Router();
-router.post("/checkout-session/:doctorId",authenticate,restrict(["patient","admin"]),getCheckoutSession);
+router.post("/checkout-session/:doctorId",authenticate,restrict(["patient","admin","doctor"]),getCheckoutSession);
 export default router;
